9d0b687f27f7eaee23853dfc50dd3be5d8f73bac
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-01-27 01:21:08 +0800

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -18,18 +18,20 @@ def encrypt(pk,m):
     #print("pk is ",pk)
     r=random.randint(1,q)
     c1 = pow(g,r,p)
-    c2_interim= m*pow(pk,r)
-    c2 = pow(c2_interim,1,p)
-    print("c2 is ",c2)
     print("c1 is ", c1)
+    firstpart=pow(pk,r,p)
+    secondpart=m%p
+    c2=(firstpart*secondpart)%p
+    print("c2 is ",c2)
     return [c1,c2]
 
 def decrypt(sk,c):
     c1=c[0]
     c2=c[1]
-    denominator=c1^sk
-    fraction=c2/denominator
-    m = fraction%p
+    firstpart=c2%p
+    secondpart=(c1**-sk)%p
+    m = (firstpart*secondpart)%p
     print("Message is ",m)
     return m
+
 
