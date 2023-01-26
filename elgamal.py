25f814eb57ac7617078630c6c68184808fb75bdb
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-01-26 11:14:47 +0800

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -9,22 +9,27 @@ def keygen():
     sk = random.randint(1,p)
     pk = pow(g,sk,p)
 
+    print("pk is ", pk)
+    print("sk is ", sk)
+
     return pk,sk
 
 def encrypt(pk,m):
+    #print("pk is ",pk)
     r=random.randint(1,q)
     c1 = pow(g,r,p)
-    interim=pow(pk,r)*m
-    c2 = interim%p
-
+    c2_interim= m*pow(pk,r)
+    c2 = pow(c2_interim,1,p)
+    print("c2 is ",c2)
+    print("c1 is ", c1)
     return [c1,c2]
 
 def decrypt(sk,c):
     c1=c[0]
     c2=c[1]
-
-    interim=c2/pow(c1,sk)
-    m = pow(interim,1,p)
-
+    denominator=c1^sk
+    fraction=c2/denominator
+    m = fraction%p
+    print("Message is ",m)
     return m
 
